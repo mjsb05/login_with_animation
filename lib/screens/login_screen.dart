b11cbb1295799f81b97c4051db8c46569859711f
Mauri Sandoval <maurijsb25@gmail.com>
2025-09-04 10:18:08 -0600

    feat (login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,15 +9,67 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _isPasswordVisible = true;
+
   @override
   Widget build(BuildContext context) {
+    //para obtener el tamaño de la pantallla del disp
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: RiveAnimation.asset('assets/animated_login_character.riv'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset(
+                  'assets/animated_login_character.riv',
+                ),
+              ),
+              //Espacio entre el oso y el texto medio
+              const SizedBox(height: 10),
+              //Campo de texto del email
+              TextField(
+                //para que el teclado sepa que es email
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  prefixIcon: const Icon(Icons.email),
+                  labelText: 'Email',
+                ),
+              ),
+              const SizedBox(height: 10),
+              //Campo para la contraseña
+              TextField(
+                //para que el teclado sepa que es password
+                obscureText: _isPasswordVisible,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  prefixIcon: const Icon(Icons.lock),
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
